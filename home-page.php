53dda1c4b710d726832c8c2dbf0e0a92ae3f68cd
Amy Singleton <developer@psycharmor.org>
2018-06-03 12:44:36 -0700

    Removed Comments for the home page template

--- home-page.php
+++ home-page.php
@@ -17,14 +17,6 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'no-title' ); ?>
-				<div class="col-sm-12">
-					<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-					?>
-				</div>
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
